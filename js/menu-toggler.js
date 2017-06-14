aab9abab3690b1914044c00113a304a5bdd8b804
zbralex <5998971@gmail.com>
2017-06-14 23:12:52 +0300

    вернул старый список, кое-что поправил. Напишу позже скрипт для кнопки закрытия меню мобильной версии

--- js/menu-toggler.js
+++ js/menu-toggler.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var mainNavOpen = document.querySelector('.main-nav__toggle');
-var mainNavClose = document.querySelector('.main-nav__toggle--close');
+var mainNavClose = document.querySelector('.main-nav__item--logo');
 var toggle = document.querySelector('.main-nav__toggle');
-var mainNavList = document.querySelector('.main-nav__mobile-wrapper');
+var mainNavList = document.querySelector('.main-nav__list');
 
 mainNavOpen.addEventListener('click', function () {
   toggle.classList.toggle('main-nav__toggle--open');
   mainNavList.classList.toggle('main-nav__list--invisible');
+  mainNavClose.classList.toggle('main-nav__list-logo--visible')
 });
