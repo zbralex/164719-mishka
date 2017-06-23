b8587bb27920a2dfbd9f2f2f2d5393ccb311203f
zbralex <5998971@gmail.com>
2017-06-23 16:35:37 +0300

    добавил обработчик на кнопку вызова модального окна/travis~

--- js/menu-toggler.js
+++ js/menu-toggler.js
@@ -1,12 +1,9 @@
 'use strict';
 
-var mainNavOpen = document.querySelector('.main-nav__toggle');
-var mainNavClose = document.querySelector('.main-nav__item--logo');
-var toggle = document.querySelector('.main-nav__toggle');
-var mainNavList = document.querySelector('.main-nav__list');
+var mainNavOpen = document.querySelector('.add-to-cart');
+var buttonCart = document.querySelector('.btn');
 
-mainNavOpen.addEventListener('click', function () {
-  toggle.classList.toggle('main-nav__toggle--open');
-  mainNavList.classList.toggle('main-nav__list--invisible');
-  mainNavClose.classList.toggle('main-nav__list-logo--visible')
+buttonCart.addEventListener('click', function () {
+  mainNavOpen.classList.remove('add-to-cart--invisible');
+  mainNavOpen.classList.add('add-to-cart--visible');
 });
